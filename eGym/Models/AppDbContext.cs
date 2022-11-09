b5e63498bcd8fd3b8ce7cdb8690dcd68120a2fc8
Irina Gorlino <irinagorlino@gmail.com>
2022-11-09 17:11:53 -0300

    edicion de program y startup

---
+++ eGym/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eGym.Models
+{
+    public class AppDbContext : DbContext
+    { 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
