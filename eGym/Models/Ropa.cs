1daabd279f8f934ade9d0f47a511a0e5a582e541
Irina Gorlino <irinagorlino@gmail.com>
2022-11-12 20:11:30 -0300

    Cambios en modelo 'Ropa'

--- eGym/Models/Ropa.cs
+++ eGym/Models/Ropa.cs
@@ -8,25 +8,32 @@ namespace eGym.Models
     {
         [Key]
         public int idRopa { get; set; }
+        [Display(Name = "Nombre")]
         public string nombre { get; set; }
+        [Display(Name = "Descripción")]
         public string detalles { get; set; }
+        [Display(Name = "Precio")]
         public double precio { get; set; }
-        public string imagenRopa { get; set; }
+        [Display(Name = "Imagen")]
+        public string ? imagenRopa { get; set; }
          
         //relaciones
-        public List<Ropa_Color> ropas_colores { get; set; }
+        public List<Ropa_Color> ? ropas_colores { get; set; }
 
         //relacion-marca
+        [Display(Name = "Marca")]
         public int marcaId { get; set; }
         [ForeignKey("marcaId")]
         public Marca Marca { get; set; }
 
         //relacion-tienda
+        [Display(Name = "Tienda")]
         public int tiendaId { get; set; }
         [ForeignKey("tiendaId")]
         public Tienda Tienda { get; set; }
 
         //relacion-categoria
+        [Display(Name = "Categoría")]
         public int categoriaId { get; set; }
         [ForeignKey("categoriaId")]
         public Categoria Categoria { get; set; }
