7aaa7a59d1a044a3dd85e0a9795763de35921026
Irina Gorlino <irinagorlino@gmail.com>
2022-11-14 16:59:52 -0300

    agregue un color mas

--- eGym/Models/Ropa-Color.cs
+++ eGym/Models/Ropa-Color.cs
@@ -5,9 +5,11 @@ namespace eGym.Models
     public class Ropa_Color
     {
         public int idRopa { get; set; }
-        public Ropa ropa { get; set; }
+        [Display(Name = "Ropa")]
+        public Ropa ? ropa { get; set; }
        
        public int idColor { get; set; }
-       public Color ? color { get; set; }
+        [Display(Name = "Color")]
+        public Color ? color { get; set; }
     }
 }
