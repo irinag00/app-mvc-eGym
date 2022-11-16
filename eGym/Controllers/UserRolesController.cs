b58eb56ead37dcb9c9fcfd6c8922c4ab23dc95c9
Irina Gorlino <irinagorlino@gmail.com>
2022-11-16 15:29:04 -0300

    ediciones con respecto a diseño/administrador

---
+++ eGym/Controllers/UserRolesController.cs
@@ -0,0 +1,27 @@
+﻿using eGym.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGym.Controllers
+{
+    [Authorize(Roles = Roles.Admin)]
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesController (UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }   
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(users);
+        }
+    }
+}
