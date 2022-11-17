6f38eb68b1dcc2f753ed917e3c7e3ef6b0b7414b
Irina Gorlino <irinagorlino@gmail.com>
2022-11-17 16:29:13 -0300

    saque el importat de ropas y elimine vistas de roles porq no andaba el controlador

--- eGym/Controllers/UserRolesController.cs
+++ eGym/Controllers/UserRolesController.cs
@@ -1,4 +1,5 @@
 ﻿using eGym.Models;
+using eGym.ModelsView;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,16 +13,30 @@ namespace eGym.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserRolesController (UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public UserRolesController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
-        }   
+        }
 
         public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.ToListAsync();
-            return View(users);
+
+            var listaUsuarios = new List<UserRolesView>();
+            foreach (IdentityUser item in users)
+            {
+                var nuevoUsuario = new UserRolesView
+                {
+                    UserId = item.Id,
+                    UserName = item.UserName,
+                    Email = item.Email,
+                    Roles = new List<string>(await _userManager.GetRolesAsync(item))
+                };
+                listaUsuarios.Add(nuevoUsuario);
+            }
+
+            return View(listaUsuarios);
         }
     }
 }
